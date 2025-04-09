2957c6f13acb290a5dd072920e1cbeda1c6b32b8
devanandha <tmdevanandha973@gmai.com>
2025-04-09 16:29:14 +0530

    .env

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express');
-require('./db')
+var dotenv=require('dotenv');
 var userRoutes=require('./user')
 var productRoutes=require('./routes/productRoute')
 var app=express();
+dotenv.config();
+require('/.db')
 app.use(express.json());
+var port = process.env.port;
 var port=4000;
 
 app.use('/api',userRoutes)
